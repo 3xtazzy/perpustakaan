63bdc628874d7e73232a05d03a9993167ca840f5
dederaditya <dederaditya19@gmail.com>
2020-12-01 11:27:19 +0800

    Create main.c
    struktur awal program

---
+++ main.c
@@ -0,0 +1,17 @@
+#define <stdio.h>
+#define <stdlib.h>
+
+//Struct?
+
+int main(void) {
+  //Tes
+}
+
+void tambah_buku(void) {
+  //Fungsi nambahin buku
+}
+
+void delete_buku(void) {
+   //Fungsi delete buku
+}
+
