4716c1b194472f47b45e1cb40cc4d54843d4aad1
krishnaamp123 <74556399+krishnaamp123@users.noreply.github.com>
2020-12-15 16:48:13 +0800

    Update main.c

--- main.c
+++ main.c
@@ -126,7 +126,7 @@ void tambah_buku(void) {
   printf("Halaman     : ");
   scanf("%u", & tambahBuku.halaman);
   printf("Harga       : ");
-  scanf("%u", & tambahBuku.harga);
+  scanf("%f", & tambahBuku.harga);
 
   //Validasi input data
 
@@ -212,6 +212,49 @@ void pengembalian(void) {
 
   //Fungsi pengembalian
   printf("Test");
+}
+void lihat_buku(void) {
+  system("cls");
+  printf("\t\t\t===========================================================================");
+  printf("\n\t\t\t------------                                                   ------------ ");
+  printf("\n\t\t\t------------   | PROGRAM PERPUSTAKAAN UNIVERSITAS UDAYANA  |   ------------ ");
+  printf("\n\t\t\t------------                                                   ------------ ");
+  printf("\n\t\t\t=========================================================================== ");
+  printf("\n\t\t\t--------------------------------------------------------------------------- ");
+  printf("\n\t\t\t                        <<< Menu Lihat Buku >>>                           ");
+  printf("\n\t\t\t--------------------------------------------------------------------------- ");
+	
+  //Fungsi lihat buku
+  int ketemu = 0;
+  char judulBuku[MAKS_JUDUL_BUKU] = {0};
+  Buku tambahBuku = 
+    0
+  }; //Memanggil ke struct ke fungsi
+  FILE *filePointer = NULL;
+  int status = 0;
+  filePointer = fopen("Buku.bin","rb");
+  while(fread (&tambahBuku, sizeof(tambahBuku), 1, filePointer)){
+      printf("\n\t\t\tKode Buku = %u", tambahBuku.kode_buku);
+      printf("\n\t\t\tNama Buku = %s", tambahBuku.judul_buku);
+      printf("\t\t\tPenulis = %s", tambahBuku.penulis);
+      printf("\t\t\tJenis Buku = %s", tambahBuku.jenis_buku);
+      printf("\t\t\tHalaman = %u", tambahBuku.halaman);
+      printf("\n\t\t\tHarga = Rp%.3f", tambahBuku.harga);
+      printf("\t\t\t");
+      ketemu = 1;
+   }
+  fclose(filePointer);
+  if(!ketemu){
+      printf("\n\t\t\tNo Record");
+  }
+  printf("\n\n\t\t\tPress enter to go to main menu.....");
+  fflush(stdin);
+  getchar();
+}
+int main(void) {
+  //Tes
+  login();
+  return 0;
 }
 
 int main(void) {
